813e0bbef2c95ee97dd768073775fc1248953c64
Cesar Borja <cborjaruiz@gmail.com>
2023-05-24 13:54:26 -0600

    Testing

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-print("hello")
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, Woasdasd!</p>"
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0',debug=True)
